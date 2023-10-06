01e717a8927df1d1ce90a2bc397b2fd1c48bcdc7
must_eat <mss@tutanota.de>
2023-10-06 05:33:33 +0300

    upd: (bands/) modules/, piss.py: > add colors to the print() prompts. > redo the len() check prompts.

--- bands/modules/piss.py
+++ bands/modules/piss.py
@@ -1,4 +1,8 @@
 from bands.util import drawbox
+from bands.util import MIRCColors
+
+# pylint: disable=invalid-name
+c = MIRCColors()
 
 
 # pylint: disable=too-few-public-methods
@@ -9,22 +13,29 @@ class Piss:
     @staticmethod
     def print(core, pisser, pissee):
         if len(str(pissee)) == 0:
-            core.send_query(f"{pisser}: on who?")
+            msg = f"{c.ORANGE}{pisser}{c.LBLUE}, "
+            msg += f"{c.WHITE}on {c.YELLOW}who{c.WHITE}?{c.RES}"
+            core.send_query(msg)
             return
 
-        if len(str(pissee)) >= 20:
-            core.send_query(f"{pisser}: pissee longer than 20 chars.")
+        if len(str(pissee)) >= 30:
+            msg = f"{c.ORANGE}{pisser}{c.LBLUE}, {c.YELLOW}pissee "
+            msg += f"{c.LRED}is longer than 30 chats.{c.RES}"
+            core.send_query(msg)
             return
 
-        msg = "     ë\n"
-        msg += f"   .-║- <- {pisser} \n"
-        msg += "   ╭╰\\\n"
-        msg += "   ┊/ \\\n"
-        msg += "   ┊\n"
-        msg += f" {pissee}\n"
+        msg = f"     {c.WHITE}ë{c.RES} \n"
+        msg += f"   {c.WHITE}.-║- {c.LBLUE}<- {c.ORANGE}{pisser}{c.RES} \n"
+        msg += f"   {c.ORANGE}╭{c.LRED}╰{c.WHITE}\\{c.RES} \n"
+        msg += f"   {c.YELLOW}┊{c.WHITE}/ \\{c.RES} \n"
+        msg += f"   {c.YELLOW}┊{c.RES} \n"
+        msg += f" {c.YELLOW}{pissee}{c.RES} \n"
 
         msg = drawbox(msg, "single")
-        msg += f"{pissee} just got pissed on by {pisser}"
+        msg += f"{c.WHITE} → {c.ORANGE}{pissee} "
+        msg += f"{c.WHITE}just got {c.YELLOW}pissed on "
+        msg += f"{c.WHITE}by {c.ORANGE}{pisser}"
+        msg += f"{c.WHITE}.{c.RES}"
 
         for line in msg.split("\n"):
             core.send_query(line)
