e675ab10f478eb851524ead341eb56a1be2f6d0d
must_eat <mss@tutanota.de>
2023-10-06 05:19:45 +0300

    upd: (bands/) util.py: > MIRCColors(); have all color codes in two digits (0 pad). > add strip_color(); strip 7/8bit ansi and mirc colors. > unilen(); strip colors before calculating. > drawbox(); color the borders.

--- bands/util.py
+++ bands/util.py
@@ -7,16 +7,16 @@ class MIRCColors:
     RES = "\x0f"
     BOLD = "\x02"
 
-    WHITE = "\x030"
-    BLACK = "\x031"
-    BLUE = "\x032"
-    GREEN = "\x033"
-    LRED = "\x034"
-    BROWN = "\x035"
-    PURPLE = "\x036"
-    ORANGE = "\x037"
-    YELLOW = "\x038"
-    LGREEN = "\x039"
+    WHITE = "\x0300"
+    BLACK = "\x0301"
+    BLUE = "\x0302"
+    GREEN = "\x0303"
+    LRED = "\x0304"
+    BROWN = "\x0305"
+    PURPLE = "\x0306"
+    ORANGE = "\x0307"
+    YELLOW = "\x0308"
+    LGREEN = "\x0309"
     CYAN = "\x0310"
     LCYAN = "\x0311"
     LBLUE = "\x0312"
@@ -28,20 +28,35 @@ class MIRCColors:
         pass
 
 
+# pylint: disable=anomalous-backslash-in-string
+def strip_color(string):
+    ansi_strip = re.compile(r"(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]")
+    mirc_strip = re.compile("[\x02\x0F\x16\x1D\x1F]|\x03(\d{,2}(,\d{,2})?)?")
+
+    return mirc_strip.sub("", ansi_strip.sub("", string))
+
+
 def unilen(string):
+    string = strip_color(string)
+
     width = 0
     for i in string:
         if unicodedata.category(i)[0] in ("M", "C"):
             continue
+
         w = unicodedata.east_asian_width(i)
         if w in ("N", "Na", "H", "A"):
             width += 1
         else:
             width += 2
+
     return width
 
 
 def drawbox(string, charset):
+    # pylint: disable=invalid-name
+    c = MIRCColors()
+
     if charset == "double":
         chars = {"1": "╔", "2": "╗", "3": "╚", "4": "╝", "h": "═", "v": "║"}
     elif charset == "single":
@@ -59,7 +74,7 @@ def drawbox(string, charset):
         string = string[1:]
 
     for i, _ in enumerate(string):
-        string[i] = re.sub(r"^", chars["v"], string[i])
+        string[i] = re.sub(r"^", f"{c.PINK}{chars['v']}{c.RES}", string[i])
 
     width = 0
     for i, _ in enumerate(string):
@@ -67,10 +82,14 @@ def drawbox(string, charset):
             width = unilen(string[i])
 
     for i, _ in enumerate(string):
-        string[i] = f"{string[i]}{(width - unilen(string[i])) * ' '}{chars['v']}"
+        string[
+            i
+        ] = f"{string[i]}{(width - unilen(string[i])) * ' '}{c.PINK}{chars['v']}{c.RES}"
 
-    string.insert(0, f"{chars['1']}{chars['h'] * (width - 1)}{chars['2']}")
-    string.append(f"{chars['3']}{chars['h'] * (width - 1)}{chars['4']}")
+    string.insert(
+        0, f"{c.PINK}{chars['1']}{chars['h'] * (width - 1)}{chars['2']}{c.RES}"
+    )
+    string.append(f"{c.PINK}{chars['3']}{chars['h'] * (width - 1)}{chars['4']}{c.RES}")
 
     fin = ""
     for i, line in enumerate(string):
