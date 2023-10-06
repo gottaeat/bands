2f470c96b7115a3081acab86236daede3f2508f3
must_eat <mss@tutanota.de>
2023-10-06 05:50:50 +0300

    upd: (bands/) modules/advice.py: > pass core to _run(). > if target > core.USER_NICKLIMIT, err. > calc len w/ unilen.

--- bands/modules/advice.py
+++ bands/modules/advice.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 
+from bands.util import unilen
 from bands.util import MIRCColors
 
 # pylint: disable=invalid-name
@@ -19,16 +20,24 @@ class Advice:
         with open(self.ADV_FILE, "r", encoding="utf-8") as adv_file:
             self.adv_data = json.loads(adv_file.read())["advices"]
 
-    def _run(self, user, user_args):
+    def _run(self, core, user, user_args):
         if user_args:
             if len(user_args.split(" ")) > 1:
                 finmsg = f"{c.WHITE}{user}{c.LBLUE},{c.RES} "
                 finmsg += f"{c.LRED}multicast advice support disabled.{c.RES}"
+
                 return finmsg
 
             target = user_args
         else:
             target = user
+
+        if unilen(str(target)) > core.USER_NICKLIMIT:
+            finmsg = f"{c.WHITE}{user}{c.LBLUE},{c.RES} "
+            finmsg += f"{c.LRED}person in need of advice is wider than "
+            finmsg += f"{core.USER_NICKLIMIT} chars.{c.RES}"
+
+            return finmsg
 
         self._parse_json()
 
@@ -41,7 +50,7 @@ class Advice:
         return finmsg
 
     def print(self, core, user, user_args=None):
-        msg = self._run(user, user_args)
+        msg = self._run(core, user, user_args)
 
         for line in msg.split("\n"):
             core.send_query(line)
