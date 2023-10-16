4012a8d0cfb1cbccd8de026ba1dcb56565f284e5
must_eat <mss@tutanota.de>
2023-10-16 03:13:24 +0300

    add: (bands/) ai.py: > move openai functionality from core.py to here, put under the class AI. > split parsing and rotating into two functions. > check for existence of openai keys file, if not, set self.keys to None, and   don't bump up the key_index.
    (fixes #7)

---
+++ bands/ai.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+import openai
+
+
+class AI:
+    _OPENAI_KEYS_FILE = (
+        f"{os.path.dirname(os.path.realpath(__file__))}/files/openai_keys.json"
+    )
+
+    def __init__(self):
+        self.openai = openai
+
+        self.keys = None
+        self.key_index = -1
+
+        self.run()
+
+    def run(self):
+        self.parse_json()
+        self.rotate_key()
+
+    def parse_json(self):
+        if os.path.isfile(self._OPENAI_KEYS_FILE):
+            with open(self._OPENAI_KEYS_FILE, "r", encoding="utf-8") as keys_file:
+                self.keys = json.loads(keys_file.read())["openai_keys"]
+
+    def rotate_key(self):
+        if not self.keys:
+            self.openai.api_key = None
+            return
+
+        if self.key_index >= 3:
+            self.key_index = 0
+        else:
+            self.key_index += 1
+
+        self.openai.api_key = self.keys[self.key_index]["key"]
