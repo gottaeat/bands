5f27e52c04fe501abc0be997048d915b4c54244a
must_eat <mss@tutanota.de>
2023-10-16 05:53:54 +0300

    upd: (bands/) irc.py: > if the self.core.char_limit is not defined, on join, get the JOIN line, and   calculate the max length, set that as self.core.char_limit. > call strip_user() instead of doing the regex here. > ?tarot cmd; pass Tarot() the Core().

--- bands/irc.py
+++ bands/irc.py
@@ -1,7 +1,7 @@
-import re
 import time
 
 from .util import strip_color
+from .util import strip_user
 
 from .modules.advice import Advice
 from .modules.finance import Finance
@@ -45,10 +45,22 @@ class IRC:
             if data.split()[0] == "PING":
                 self.core.send_pong()
 
+            if not self.core.char_limit:
+                if (
+                    data.split()[1] == "JOIN"
+                    and strip_user(data.split()[0]) == self.core.botname
+                    and data.split()[2] == self.core.channel
+                ):
+                    self.core.char_limit = 512 - len(
+                        f"{data.split()[0]} PRIVMSG {data.split()[2]} :\r\n".encode(
+                            "utf-8"
+                        )
+                    )
+
             if data.split()[1] == "PRIVMSG" and data.split()[2] == self.core.channel:
                 cmd = data.split()[3]
 
-                user = re.sub(r"^:|\![^!]*$", "", data.split()[0])
+                user = strip_user(data.split()[0])
                 user_args = " ".join(data.split()[4:])
 
                 if cmd == ":?openai":
@@ -64,7 +76,7 @@ class IRC:
                     Piss(self.core).print(user, user_args)
 
                 if cmd == ":?tarot":
-                    retval = Tarot().print(self.tarot_deck, user_args)
+                    retval = Tarot(self.core).print(self.tarot_deck, user_args)
 
                     try:
                         if retval[0].__class__.__name__ == "TarotCard":
