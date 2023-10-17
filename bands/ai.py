c0df8163f444bc08403258aad8bb3fd38404332b
must_eat <mss@tutanota.de>
2023-10-17 10:41:43 +0300

    upd: (bands/) ai.py: > remove _OPENAI_KEYS_FILE const, we take that in from the CLI parsed YAML. > dont run() in __init__. > raise exception if keys_file parsing fails and never set the api_key to None,   we force enable openai functionality for now. > checks for the existence of file is done via CLI now.

--- bands/ai.py
+++ bands/ai.py
@@ -1,41 +1,33 @@
 import json
-import os
 
 import openai
 
 
 class AI:
-    _OPENAI_KEYS_FILE = (
-        f"{os.path.dirname(os.path.realpath(__file__))}/files/openai_keys.json"
-    )
-
     def __init__(self):
-        self.openai = openai
+        self.keys_file = None
 
         self.keys = None
         self.key_index = -1
 
-        self.run()
-
-    def run(self):
-        self.parse_json()
-        self.rotate_key()
+        self.openai = openai
 
     def parse_json(self):
-        if os.path.isfile(self._OPENAI_KEYS_FILE):
-            with open(self._OPENAI_KEYS_FILE, "r", encoding="utf-8") as keys_file:
+        try:
+            with open(self.keys_file, "r", encoding="utf-8") as keys_file:
                 self.keys = json.loads(keys_file.read())["openai_keys"]
-        else:
-            self.keys = None
+        except Exception as exc:
+            # pylint: disable=raise-missing-from
+            raise ValueError(f"E: parsing {self.keys_file} failed:\n{exc}")
 
     def rotate_key(self):
-        if not self.keys:
-            self.openai.api_key = None
-            return
-
         if self.key_index >= 3:
             self.key_index = 0
         else:
             self.key_index += 1
 
         self.openai.api_key = self.keys[self.key_index]["key"]
+
+    def run(self):
+        self.parse_json()
+        self.rotate_key()
