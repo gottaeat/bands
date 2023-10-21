40dfa118d15e45bcfc5fb6b79e1cad8c7c4c4b39
must_eat <mss@tutanota.de>
2023-10-22 01:33:28 +0300

    rwr: (bands/) ai.py: > AI class:  - init an openai instance.  - store keys and a key index.  - allow rotatation to the next key.

--- bands/ai.py
+++ bands/ai.py
@@ -1,33 +1,21 @@
-import json
-
 import openai
 
 
+# pylint: disable=too-few-public-methods
 class AI:
     def __init__(self):
-        self.keys_file = None
+        self.openai = openai
 
         self.keys = None
         self.key_index = -1
 
-        self.openai = openai
-
-    def parse_json(self):
-        try:
-            with open(self.keys_file, "r", encoding="utf-8") as keys_file:
-                self.keys = json.loads(keys_file.read())["openai_keys"]
-        except Exception as exc:
-            # pylint: disable=raise-missing-from
-            raise ValueError(f"E: parsing {self.keys_file} failed:\n{exc}")
-
     def rotate_key(self):
-        if self.key_index >= 3:
-            self.key_index = 0
+        if self.keys:
+            if self.key_index >= 3:
+                self.key_index = 0
+            else:
+                self.key_index += 1
+
+            self.openai.api_key = self.keys[self.key_index]["key"]
         else:
-            self.key_index += 1
-
-        self.openai.api_key = self.keys[self.key_index]["key"]
-
-    def run(self):
-        self.parse_json()
-        self.rotate_key()
+            self.openai.api_key = None
