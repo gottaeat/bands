e36b412ff924a3cd1ec95540fefb2f887b897e43
must_eat <mss@tutanota.de>
2023-10-22 02:01:16 +0300

    add: (bands/) user.py: > User class:  - Channel() class without the tarot_deck, seperated for possible future    diversions.

---
+++ bands/user.py
@@ -0,0 +1,29 @@
+import time
+
+from .util import wrap_bytes
+
+
+# pylint: disable=too-few-public-methods
+class User:
+    def __init__(self, server):
+        self.server = server
+        self.name = None
+        self.char_limit = None
+
+    def send_query(self, msg):
+        if "\n" in msg:
+            for line in msg.split("\n"):
+                if line != "":
+                    if len(line.encode("utf-8")) > self.char_limit:
+                        for item in wrap_bytes(line, self.char_limit):
+                            self.server.send_raw(f"PRIVMSG {self.name} :{item}")
+                            time.sleep(self.server.scroll_speed)
+                    else:
+                        self.server.send_raw(f"PRIVMSG {self.name} :{line}")
+        else:
+            if len(msg.encode("utf-8")) > self.char_limit:
+                for item in wrap_bytes(msg, self.char_limit):
+                    self.server.send_raw(f"PRIVMSG {self.name} :{item}")
+                    time.sleep(self.server.scroll_speed)
+            else:
+                self.server.send_raw(f"PRIVMSG {self.name} :{msg}")
