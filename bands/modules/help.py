7360f9229589974b6a38c786ea3665a499159c23
must_eat <mss@tutanota.de>
2023-10-22 02:44:11 +0300

    upd: (bands/) modules/, help.py: > take in a channel object in class Help(). > redo the the prompt. > core.send_query() -> self.channel.send_query().

--- bands/modules/help.py
+++ bands/modules/help.py
@@ -7,14 +7,20 @@ c = MIRCColors()
 
 # pylint: disable=too-few-public-methods
 class Help:
-    def __init__(self, core):
-        self.core = core
+    def __init__(self, channel):
+        self.channel = channel
 
     def print(self):
-        msg = f"{c.WHITE}help{c.RES}\n"
-        msg += f"{c.WHITE}├ {c.LRED}usage {c.LBLUE}→{c.RES} {{?help|?bands|?tarot}}\n"
-        msg += f"{c.WHITE}│       {c.LBLUE}→{c.RES} {{?piss [target]}}\n"
-        msg += f"{c.WHITE}│       {c.LBLUE}→{c.RES} {{?advice {{target}}}}\n"
-        msg += f"{c.WHITE}└ {c.LRED}src   {c.LBLUE}→{c.RES} https://github.com/gottaeat/bands"
+        msg = f"{c.WHITE}├ {c.LRED}usage{c.RES}\n"
+        msg += f"{c.WHITE}│ ├ {c.LRED}channel{c.RES}\n"
+        msg += f"{c.WHITE}│ │ ├ {c.LGREEN}?advice{c.RES} {{target}}\n"
+        msg += f"{c.WHITE}│ │ ├ {c.LGREEN}?bands{c.RES}\n"
+        msg += f"{c.WHITE}│ │ ├ {c.LGREEN}?help{c.RES}\n"
+        msg += f"{c.WHITE}│ │ ├ {c.LGREEN}?piss{c.RES}   [target]\n"
+        msg += f"{c.WHITE}│ │ └ {c.LGREEN}?tarot{c.RES}  {{last}}\n"
+        msg += f"{c.WHITE}│ └ {c.LRED}user{c.RES}\n"
+        msg += f"{c.WHITE}│   ├ {c.LGREEN}?auth{c.RES}   [secret]\n"
+        msg += f"{c.WHITE}│   └ {c.LGREEN}?openai{c.RES} [reload|status]\n"
+        msg += f"{c.WHITE}└ {c.LRED}src{c.RES}         https://github.com/gottaeat/bands"
 
-        self.core.send_query(drawbox(msg, "thic"))
+        self.channel.send_query(msg)
