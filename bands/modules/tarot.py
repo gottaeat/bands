5dc2d3341a44f00b77a9aaa3ca5c8bd5f9b90a6c
must_eat <mss@tutanota.de>
2023-10-29 06:10:00 +0300

    upd: (bands/) modules/, tarot.py; remove mutex lock logic for the AI() interactions, unnecessary / just causes extra waiting without giving us an advantage when GIL is a thing.

--- bands/modules/tarot.py
+++ bands/modules/tarot.py
@@ -89,10 +89,6 @@ class Tarot:
         for index, card in enumerate(deck.cards):
             cards_str += f"{index+1}. {card.title}\n"
 
-        # get current key index
-        with self.channel.server.ai.mutex:
-            key_index = self.channel.server.ai.key_index
-
         # generate prompt
         prompt = f'The querent\'s question is "{deck.question}". Read the '
         prompt += "following Celtic tarot deck and answer the querent's "
@@ -113,15 +109,15 @@ class Tarot:
         ]
 
         # notify that we're generating the reading
-        notif_msg = f"{c.GREEN}[{c.LBLUE}I:{key_index}{c.GREEN}] "
+        notif_msg = f"{c.GREEN}[{c.LBLUE}I:"
+        notif_msg += f"{self.channel.server.ai.key_index}{c.GREEN}] "
         notif_msg += f"{c.LGREEN}Generating reading for "
         notif_msg += f"{c.WHITE}{self.user.name}{c.LBLUE}.{c.RES}"
 
         self.channel.send_query(notif_msg)
 
         # rotate before call
-        with self.channel.server.ai.mutex:
-            self.channel.server.ai.rotate_key()
+        self.channel.server.ai.rotate_key()
 
         # call openai api
         # the thread safety issue is only within the scope of the pure python
