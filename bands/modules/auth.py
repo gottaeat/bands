d171289c2b7737c330ca33afdc42f520ddecedf3
must_eat <mss@tutanota.de>
2023-11-15 02:57:20 +0300

    upd: (bands/) modules/, auth.py; use precrafted prompts for prompts.

--- bands/modules/auth.py
+++ bands/modules/auth.py
@@ -18,10 +18,8 @@ class Auth:
                 "%s ran ?auth without providing a secret", self.user.name
             )
 
-            errmsg = f"{c.GREEN}[{c.LRED}E{c.GREEN}] "
-            errmsg += f"{c.LRED}a secret is necessary.{c.RES}"
+            errmsg = f"{c.ERR} a secret is necessary."
             self.user.send_query(errmsg)
-
             return
 
         if self.user_args[0] == self.user.server.secret:
@@ -31,15 +29,12 @@ class Auth:
                 "%s now has admin perms in %s", self.user.name, self.user.server.name
             )
 
-            msg = f"{c.GREEN}[{c.LBLUE}I{c.GREEN}] "
-            msg += f"{c.LGREEN}{self.user.name} {c.WHITE}now has {c.LRED}admin "
-            msg += f"perms {c.WHITE}in {c.GREEN}{self.user.server.name}.{c.RES}"
+            msg = f"{c.INFO} you are now authorized as the admin user "
+            msg += f"for {self.user.server.name}."
             self.user.send_query(msg)
-
             return
 
         self.user.server.logger.warning("%s provided incorrect secret", self.user.name)
 
-        errmsg = f"{c.GREEN}[{c.LRED}E{c.GREEN}] "
-        errmsg += f"{c.LRED}secret is incorrect.{c.RES}"
+        errmsg = f"{c.ERR} incorrect secret provided."
         self.user.send_query(errmsg)
