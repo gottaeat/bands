3f4a1d870cba64351491f71e3d9ce10aa56003fc
must_eat <mss@tutanota.de>
2023-11-15 02:54:11 +0300

    rwr: (bands/) modules/, advice.py; split to individual query sending and use precrafted prompts for errmsg's.

--- bands/modules/advice.py
+++ bands/modules/advice.py
@@ -19,38 +19,38 @@ class Advice:
         self.user_args = user_args
 
         self.adv_data = None
+        self.advice = None
 
-        self.channel.send_query(self._run())
+        self._run()
 
     def _parse_json(self):
         with open(self.ADV_FILE, "r", encoding="utf-8") as adv_file:
             self.adv_data = json.loads(adv_file.read())["advices"]
 
+    def _pull(self):
+        random.shuffle(self.adv_data)
+        self.advice = self.adv_data.pop(random.randrange(len(self.adv_data)))
+
     def _run(self):
         if len(self.user_args) > 0:
             if len(self.user_args) > 1:
-                finmsg = f"{c.WHITE}{self.user.name}{c.LBLUE},{c.RES} "
-                finmsg += f"{c.LRED}multicast advice support disabled.{c.RES}"
-
-                return finmsg
+                errmsg = f"{c.ERR} multicast advice support is disabled."
+                self.channel.send_query(errmsg)
+                return
 
             target = self.user_args[0]
         else:
             target = self.user.name
 
         if unilen(target) > self.channel.server.USER_NICKLIMIT:
-            finmsg = f"{c.WHITE}{self.user.name}{c.LBLUE},{c.RES} "
-            finmsg += f"{c.LRED}person in need of advice is wider than "
-            finmsg += f"{self.channel.server.USER_NICKLIMIT} chars.{c.RES}"
-
-            return finmsg
+            errmsg = f"{c.ERR} person in need of advice is wider than "
+            errmsg += f"{self.channel.server.USER_NICKLIMIT} chars."
+            self.channel.send_query(errmsg)
+            return
 
         self._parse_json()
+        self._pull()
 
-        random.shuffle(self.adv_data)
-        advice = self.adv_data.pop(random.randrange(len(self.adv_data)))
-
-        finmsg = f"{c.WHITE}{target}{c.LBLUE},{c.RES} "
-        finmsg += f"{c.GREEN}{advice}{c.RES}\n"
-
-        return finmsg
+        msg = f"{c.WHITE}{target}{c.LBLUE},{c.RES} "
+        msg += f"{c.GREEN}{self.advice}{c.RES}\n"
+        self.channel.send_query(msg)
