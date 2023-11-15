e5c7a8eb985b21f8e72d9323f4d720b4a337fc5c
must_eat <mss@tutanota.de>
2023-11-15 03:11:39 +0300

    upd: (bands/) modules/; split help.py to ChannelHelp() and UserHelp().

---
+++ bands/modules/channel_help.py
@@ -0,0 +1,29 @@
+from bands.colors import MIRCColors
+
+# pylint: disable=invalid-name
+c = MIRCColors()
+
+
+# pylint: disable=too-few-public-methods
+class ChannelHelp:
+    def __init__(self, channel, user, user_args):
+        self.channel = channel
+        self.user = user  # unused
+        self.user_args = user_args  # unused
+
+        self._run()
+
+    def _run(self):
+        msg = f"{c.WHITE}├ {c.LRED}usage\n"
+        msg += f"{c.WHITE}│ ├ {c.LGREEN}?advice{c.RES} {target}\n"
+        msg += f"{c.WHITE}│ ├ {c.LGREEN}?bands{c.RES}\n"
+        msg += f"{c.WHITE}│ ├ {c.LGREEN}?help{c.RES}\n"
+        msg += f"{c.WHITE}│ ├ {c.LGREEN}?piss{c.RES}   [target]\n"
+        msg += (
+            f"{c.WHITE}│ └ {c.LGREEN}?tarot{c.RES}  has its own detailed help prompt\n"
+        )
+        msg += (
+            f"{c.WHITE}└ {c.LRED}src{c.RES}       https://github.com/gottaeat/bands\n"
+        )
+
+        self.channel.send_query(msg)
---
+++ bands/modules/user_help.py
@@ -0,0 +1,30 @@
+from bands.colors import MIRCColors
+
+# pylint: disable=invalid-name
+c = MIRCColors()
+
+
+# pylint: disable=too-few-public-methods
+class UserHelp:
+    def __init__(self, user, user_args):
+        self.user = user
+        self.user_args = user_args
+
+        self._run()
+
+    def _run(self):
+        msg = "{c.WHITE}├ {c.LRED}usage{c.RES}\n"
+
+        if self.user.name == self.user.server.admin:
+            msg += "{c.WHITE}│  ├ {c.LGREEN}?auth{c.RES}   [secret]\n"
+            msg += "{c.WHITE}│  └ {c.LGREEN}?openai{c.RES} [load|status]\n"
+            msg += (
+                "{c.WHITE}└ {c.LRED}src{c.RES}        https://github.com/gottaeat/bands"
+            )
+        else:
+            msg += "{c.WHITE}│  └ {c.LGREEN}?auth{c.RES} [secret]\n"
+            msg += (
+                "{c.WHITE}└ {c.LRED}src{c.RES}      https://github.com/gottaeat/bands"
+            )
+
+        self.user.send_query(msg)
