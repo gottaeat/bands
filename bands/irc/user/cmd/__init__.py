ca1a5794293424ee68c3113d2026b7fe32627455
must_eat <mss@tutanota.de>
2023-12-01 15:43:37 +0300

    rwr: (bands/) irc/user/cmd/, __init__.py; directly expose CMDS as an attrib.

--- bands/irc/user/cmd/__init__.py
+++ bands/irc/user/cmd/__init__.py
@@ -2,3 +2,10 @@ from .auth import Auth
 from .help import Help
 from .openai_handler import OpenAIHandler
 from .rcon import RCon
+
+CMDS = {
+    ":?auth": Auth,
+    ":?help": Help,
+    ":?openai": OpenAIHandler,
+    ":?rcon": RCon,
+}
