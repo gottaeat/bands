6e3ec0c5f8d4c6d08bb2c0c40b6a45651111009d
must_eat <mss@tutanota.de>
2023-12-01 15:43:20 +0300

    rwr: (bands/) irc/channel/cmd/, __init__.py; directly expose CMDS as an attrib.

--- bands/irc/channel/cmd/__init__.py
+++ bands/irc/channel/cmd/__init__.py
@@ -3,3 +3,11 @@ from .finance import Finance
 from .help import Help
 from .piss import Piss
 from .tarot import Tarot
+
+CMDS = {
+    ":?advice": Advice,
+    ":?bands": Finance,
+    ":?help": Help,
+    ":?piss": Piss,
+    ":?tarot": Tarot,
+}
