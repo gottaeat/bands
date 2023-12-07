a7dcba8cb0694680ee7a1d77c255d3fd372d9306
must_eat <mss@tutanota.de>
2023-12-08 00:15:25 +0300

    upd: (bands/) irc/channel/channel.py: > add ChannelUser class. > add user_list attrib to Channel objects, stores ChannelUser instances.

--- bands/irc/channel/channel.py
+++ bands/irc/channel/channel.py
@@ -15,6 +15,8 @@ class Channel:
         self.topic_user = None
         self.topic_tstamp = None
 
+        self.user_list = []
+
         self.char_limit = None
         self.tstamp = None
 
@@ -35,3 +37,21 @@ class Channel:
                     time.sleep(self.server.scroll_speed)
             else:
                 self.sock_ops.send_raw(f"PRIVMSG {self.name} :{msg}")
+
+
+class ChannelUser:
+    def __init__(self):
+        self.nick = None
+        self.ircname = None
+        self.hostname = None
+        self.ident = None
+
+        # modes
+        self.owner = None  # ~
+        self.admin = None  # &
+        self.op = None  # @
+        self.hop = None  # %
+        self.voiced = None  # +
+
+        # context
+        self.tarot_deck = None
