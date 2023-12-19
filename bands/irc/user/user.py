3c67b33c998d15d43a1f2cfa9c34a54d388b8bb4
must_eat <mss@tutanota.de>
2023-12-20 01:39:13 +0300

    upd: (bands/) irc/user/user.py: > split name attrib to nick and login. > send_query(); self.name -> self.nick.

--- bands/irc/user/user.py
+++ bands/irc/user/user.py
@@ -9,7 +9,8 @@ class User:
         self.server = server
         self.sock_ops = server.sock_ops
 
-        self.name = None
+        self.nick = None
+        self.login = None
 
         self.char_limit = None
         self.tarot_deck = None
@@ -22,14 +23,14 @@ class User:
                 if line != "":
                     if len(line.encode("utf-8")) > self.char_limit:
                         for item in wrap_bytes(line, self.char_limit):
-                            self.sock_ops.send_raw(f"PRIVMSG {self.name} :{item}")
+                            self.sock_ops.send_raw(f"PRIVMSG {self.nick} :{item}")
                             time.sleep(self.server.scroll_speed)
                     else:
-                        self.sock_ops.send_raw(f"PRIVMSG {self.name} :{line}")
+                        self.sock_ops.send_raw(f"PRIVMSG {self.nick} :{line}")
         else:
             if len(msg.encode("utf-8")) > self.char_limit:
                 for item in wrap_bytes(msg, self.char_limit):
-                    self.sock_ops.send_raw(f"PRIVMSG {self.name} :{item}")
+                    self.sock_ops.send_raw(f"PRIVMSG {self.nick} :{item}")
                     time.sleep(self.server.scroll_speed)
             else:
-                self.sock_ops.send_raw(f"PRIVMSG {self.name} :{msg}")
+                self.sock_ops.send_raw(f"PRIVMSG {self.nick} :{msg}")
