6cbe42a420ba3ac8a6b0a4e09fe1f57db1abe9c3
must_eat <mss@tutanota.de>
2024-02-05 06:36:42 +0300

    upd: (bands/) irc/user/cmd/auth.py; implement authentication lockout. (fixes #48)

--- bands/irc/user/cmd/auth.py
+++ bands/irc/user/cmd/auth.py
@@ -13,17 +13,47 @@ class Auth:
         self._run()
 
     def _run(self):
-        if len(self.user_args) == 0:
+        # auth is disabled server-wide
+        if self.user.server.bad_pw_attempts >= 12:
             self.user.server.logger.warning(
-                "%s (%s) ran ?auth without providing a secret",
+                "%s (%s) tried to auth when authentication is disabled",
                 self.user.nick,
                 self.user.login,
             )
 
-            errmsg = f"{c.ERR} a secret is necessary."
+            errmsg = f"{c.ERR} authentication is disabled."
             self.user.send_query(errmsg)
             return
 
+        # auth is disabled for user
+        if self.user.bad_pw_attempts >= 3:
+            self.user.server.logger.warning(
+                "%s (%s) failed 3 times in total when trying to authenticate",
+                self.user.nick,
+                self.user.login,
+            )
+
+            errmsg = f"{c.ERR} authentication is disabled."
+            self.user.send_query(errmsg)
+            return
+
+        # user provided no password
+        if len(self.user_args) == 0:
+            self.user.bad_pw_attempts += 1
+            self.user.server.bad_pw_attempts += 1
+
+            self.user.server.logger.warning(
+                "%s (%s) ran ?auth without providing a secret (%s/3)",
+                self.user.nick,
+                self.user.login,
+                self.user.bad_pw_attempts,
+            )
+
+            errmsg = f"{c.ERR} a secret is necessary. ({self.user.bad_pw_attempts}/3)"
+            self.user.send_query(errmsg)
+            return
+
+        # user provided correct pass
         if self.user_args[0] == self.user.server.secret:
             self.user.server.admin = self.user
 
@@ -39,9 +69,16 @@ class Auth:
             self.user.send_query(msg)
             return
 
+        # user provided wrong pass
+        self.user.bad_pw_attempts += 1
+        self.user.server.bad_pw_attempts += 1
+
         self.user.server.logger.warning(
-            "%s (%s) provided incorrect secret", self.user.nick, self.user.login
+            "%s (%s) provided incorrect secret (%s/3)",
+            self.user.nick,
+            self.user.login,
+            self.user.bad_pw_attempts,
         )
 
-        errmsg = f"{c.ERR} incorrect secret provided."
+        errmsg = f"{c.ERR} incorrect secret provided. ({self.user.bad_pw_attempts}/3)"
         self.user.send_query(errmsg)
