5bc49b53744c8292c8b47ad248fa0e1e2b5c421c
must_eat <mss@tutanota.de>
2024-02-11 03:54:18 +0300

    rwr: (bands/) irc/user/cmd/rcon.py; > add functionality to allow the bot to JOIN and PART from any given connected   server. > make raw irc sending server agnostic. > add _get_objects(); take a server name, return its object and a dict of all   its channels where keys are channel's name and its object is the val. > status cmd now prints none if we are not connected to any channel. > shorten prompts, remove extra prompt lines, call send_query w/ msg directly. > remove unnecessary space in the usage prompt. > if the user is not authenticated, simply do not respond. > _* -> _cmd_* if it's a cmd that gets exposed to the user via an arg.

--- bands/irc/user/cmd/rcon.py
+++ bands/irc/user/cmd/rcon.py
@@ -13,12 +13,166 @@ class RCon:
         self._run()
 
     def _usage(self):
-        msg = f"{c.WHITE}  ├ {c.LGREEN}dc{c.RES}     [server name]\n"
-        msg += f"{c.WHITE}  ├ {c.LGREEN}raw{c.RES}    [raw irc line]\n"
-        msg += f"{c.WHITE}  └ {c.LGREEN}status{c.RES}"
+        msg = f"{c.WHITE}├ {c.LGREEN}dc{c.RES}    [list of servers]\n"
+        msg += f"{c.WHITE}├ {c.LGREEN}join{c.RES}  [server] [list of channels]\n"
+        msg += f"{c.WHITE}├ {c.LGREEN}part{c.RES}  [server] [list of channels]\n"
+        msg += f"{c.WHITE}├ {c.LGREEN}raw{c.RES}   [raw irc line]\n"
+        msg += f"{c.WHITE}└ {c.LGREEN}status{c.RES}"
         self.user.send_query(msg)
 
-    def _status(self):
+    # pylint: disable=too-many-return-statements
+    def _run(self):
+        if self.user != self.user.server.admin:
+            return
+
+        if len(self.user_args) == 0:
+            self._usage()
+            return
+
+        if self.user_args[0] == "dc":
+            self._cmd_dc()
+            return
+
+        if self.user_args[0] == "join":
+            self._cmd_join()
+            return
+
+        if self.user_args[0] == "part":
+            self._cmd_part()
+            return
+
+        if self.user_args[0] == "raw":
+            self._cmd_raw()
+            return
+
+        if self.user_args[0] == "status":
+            self._cmd_status()
+            return
+
+        self._usage()
+
+    # -- for _cmd_join() + _cmd_part() -- #
+    def _get_objects(self, server_name):
+        # server object
+        for server in self.user.server.cli.servers:
+            if server.name == server_name:
+                server_obj = server
+                break
+
+        try:
+            if server_obj:
+                # channel name: channel object
+                sv_chans = {}
+                for chan in server_obj.channel_obj:
+                    sv_chans[chan.name] = chan
+        except NameError:
+            server_obj, sv_chans = False, False
+
+        return server_obj, sv_chans
+
+    # -- cmds -- #
+    def _cmd_dc(self):
+        server_names = self.user_args[1:]
+
+        if len(server_names) == 0:
+            self.user.send_query(f"{c.ERR} must supply at least one server.")
+            return
+
+        sv_list = []
+        sv_objs = {}
+        for server in self.user.server.cli.servers:
+            sv_list.append(server.name)
+            sv_objs[server.name] = server
+
+        for server in server_names:
+            if server not in sv_list:
+                self.user.send_query(f"{c.ERR} not connected to {server}.")
+                return
+
+            self.user.send_query(f"{c.INFO} disconnecting from {server}")
+            sv_objs[server].stop()
+
+    def _cmd_join(self):
+        # take args
+        server_name = self.user_args[1]
+        channel_names = self.user_args[2:]
+
+        if len(server_name) == 0:
+            self.user.send_query(f"{c.ERR} must supply a server as the first arg.")
+            return
+
+        if len(channel_names) == 0:
+            self.user.send_query(f"{c.ERR} must supply at least one channel.")
+            return
+
+        # get server object
+        server_obj, sv_chans = self._get_objects(server_name)
+
+        # no server object
+        if not server_obj:
+            self.user.send_query(f"{c.ERR} not connected to {server_name}.")
+            return
+
+        # check if channel already exists
+        for channel_name in channel_names:
+            # pylint: disable=consider-iterating-dictionary
+            if channel_name in sv_chans.keys():
+                self.user.send_query(
+                    f"{c.ERR} already in {channel_name} in {server_obj.name}."
+                )
+                return
+
+        # join channel(s)
+        for channel_name in channel_names:
+            server_obj.sock_ops.send_join(channel_name)
+            self.user.send_query(f"{c.INFO} sent JOIN for {channel_name}.")
+
+    def _cmd_part(self):
+        # take args
+        server_name = self.user_args[1]
+        channel_names = self.user_args[2:]
+
+        if len(server_name) == 0:
+            self.user.send_query(f"{c.ERR} must supply a server as the first arg.")
+            return
+
+        if len(channel_names) == 0:
+            self.user.send_query(f"{c.ERR} must supply at least one channel.")
+            return
+
+        # get server object
+        server_obj, sv_chans = self._get_objects(server_name)
+
+        # no server object
+        if not server_obj:
+            self.user.send_query(f"{c.ERR} not connected to {server_name}.")
+            return
+
+        # check if channel already exists
+        for channel_name in channel_names:
+            # pylint: disable=consider-iterating-dictionary
+            if channel_name not in sv_chans.keys():
+                self.user.send_query(
+                    f"{c.ERR} not in {channel_name} in {server_obj.name}."
+                )
+                return
+
+        # part from channel(s)
+        for channel_name in channel_names:
+            server_obj.sock_ops.send_part(channel_name, "mom said no")
+            self.user.send_query(f"{c.INFO} parted from {channel_name}")
+
+    def _cmd_raw(self):
+        msg = " ".join(self.user_args[1:])
+
+        if len(msg) == 0:
+            self.user.send_query(f"{c.ERR} no message provided.")
+            return
+
+        self.user.sock_ops.send_raw(msg)
+
+    # pylint: disable=too-many-branches
+    def _cmd_status(self):
         msg = f"{c.INFO} active connections are:\n"
 
         for server in self.user.server.cli.servers:
@@ -30,97 +184,46 @@ class RCon:
             else:
                 msg += "no auth\n"
 
-            msg += f"{c.WHITE}└ {c.LGREEN}channels:\n"
+            msg += f"{c.WHITE}└ {c.LGREEN}channels:{c.RES}"
 
-            for chan in server.channel_obj:
-                msg += f"  {c.LRED}→ {c.LGREEN}{chan.name}{c.RES}\n"
+            if len(server.channel_obj) == 0:
+                msg += " none\n"
 
-                if chan.topic_msg:
-                    msg += f"    {c.WHITE}├ {c.YELLOW}topic  {c.RES}{chan.topic_msg}\n"
-                    msg += f"    {c.WHITE}├ {c.YELLOW}set by {c.RES}{chan.topic_nick} "
-                    msg += f"({chan.topic_login})\n"
-                    msg += (
-                        f"    {c.WHITE}├ {c.YELLOW}date   {c.RES}{chan.topic_tstamp}\n"
-                    )
-                else:
-                    msg += f"    {c.WHITE}├ {c.YELLOW}topic  {c.RES}not set\n"
+            else:
+                msg += "\n"
+                for chan in server.channel_obj:
+                    msg += f"  {c.LRED}→ {c.LGREEN}{chan.name}{c.RES}\n"
 
-                msg += f"    {c.WHITE}└ {c.YELLOW}users: \n"
+                    if chan.topic_msg:
+                        msg += (
+                            f"    {c.WHITE}├ {c.YELLOW}topic  {c.RES}{chan.topic_msg}\n"
+                        )
+                        msg += (
+                            f"    {c.WHITE}├ {c.YELLOW}set by {c.RES}{chan.topic_nick} "
+                        )
+                        msg += f"({chan.topic_login})\n"
+                        msg += f"    {c.WHITE}├ {c.YELLOW}date   {c.RES}{chan.topic_tstamp}\n"
+                    else:
+                        msg += f"    {c.WHITE}├ {c.YELLOW}topic  {c.RES}not set\n"
 
-                for user in chan.user_list:
-                    userstr = ""
+                    msg += f"    {c.WHITE}└ {c.YELLOW}users: \n"
 
-                    if user.owner:
-                        userstr += "~"
-                    if user.admin:
-                        userstr += "&"
-                    if user.op:
-                        userstr += "@"
-                    if user.hop:
-                        userstr += "%"
-                    if user.voiced:
-                        userstr += "+"
+                    for user in chan.user_list:
+                        userstr = ""
 
-                    userstr += f"{user.nick} ({user.login})"
+                        if user.owner:
+                            userstr += "~"
+                        if user.admin:
+                            userstr += "&"
+                        if user.op:
+                            userstr += "@"
+                        if user.hop:
+                            userstr += "%"
+                        if user.voiced:
+                            userstr += "+"
 
-                    msg += f"      {c.LRED}→{c.RES} {userstr} \n"
+                        userstr += f"{user.nick} ({user.login})"
+
+                        msg += f"      {c.LRED}→{c.RES} {userstr} \n"
 
         self.user.send_query(msg)
-
-    def _raw(self):
-        msg = " ".join(self.user_args[1:])
-
-        if len(msg) == 0:
-            errmsg = f"{c.ERR} no message provided."
-            self.user.send_query(errmsg)
-            return
-
-        self.user.sock_ops.send_raw(msg)
-
-    def _dc(self):
-        server_names = self.user_args[1:]
-
-        if len(server_names) == 0:
-            errmsg = f"{c.ERR} must supply at least one server."
-            self.user.send_query(errmsg)
-            return
-
-        sv_list = []
-        sv_objs = {}
-        for server in self.user.server.cli.servers:
-            sv_list.append(server.name)
-            sv_objs[server.name] = server
-
-        for server in server_names:
-            if server not in sv_list:
-                errmsg = f"{c.ERR} not connected to {server}."
-                self.user.send_query(errmsg)
-                return
-
-            msg = f"{c.INFO} disconnecting from {server}"
-            self.user.send_query(msg)
-            sv_objs[server].stop()
-
-    def _run(self):
-        if self.user != self.user.server.admin:
-            errmsg = f"{c.ERR} not authorized."
-            self.user.send_query(errmsg)
-            return
-
-        if len(self.user_args) == 0:
-            self._usage()
-            return
-
-        if self.user_args[0] == "status":
-            self._status()
-            return
-
-        if self.user_args[0] == "dc":
-            self._dc()
-            return
-
-        if self.user_args[0] == "raw":
-            self._raw()
-            return
-
-        self._usage()
