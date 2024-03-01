1a85d7000c31b37e95253b39bf6984d399c31094
must_eat <mss@tutanota.de>
2024-03-01 04:00:15 +0300

    upd: (bands/) doot.py: > move the dooting up | down mechanism here:  - takes in a server and channeluser object.  - returns the doot state for matching channeluser.

--- bands/doot.py
+++ bands/doot.py
@@ -45,3 +45,41 @@ class Doot:
 
         with open(self.file, "w", encoding="utf-8") as file:
             file.write(json.dumps(doots))
+
+    # generic for other shits to alter doots as well
+    # takes a ChannelUser object !
+    def alter_doot(self, server, channel_user, amount):
+        with self.mutex:
+            doots = self.read_doots()
+
+            # entry for server does not exist
+            if server.name not in doots["doots"][0].keys():
+                doots["doots"][0][server.name] = []
+
+                server.logger.info("created doot entry for: %s", server.name)
+
+            # find user entry
+            user_exists = (False, None)
+            for index, doot_user_entry in enumerate(doots["doots"][0][server.name]):
+                if doot_user_entry["nick"].lower() == channel_user.nick.lower():
+                    user_exists = (True, index)
+                    break
+
+            # user does not exist, initialize it
+            if not user_exists[0]:
+                dooted_user_dict = {"nick": channel_user.nick, "doots": amount}
+
+                doots["doots"][0][server.name].append(dooted_user_dict)
+            # user exists
+            else:
+                doots["doots"][0][server.name][user_exists[1]]["doots"] += amount
+
+            self.write_doots(doots)
+
+        # prompt
+        if user_exists[0]:
+            user_doots = doots["doots"][0][server.name][user_exists[1]]["doots"]
+        else:
+            user_doots = dooted_user_dict["doots"]
+
+        return user_doots
