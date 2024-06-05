4d0cbdb97dcf9fc4a642c8f38313735d413b6669
must_eat <mss@tutanota.de>
2024-06-05 20:39:11 +0300

    rwr: (bands/) config.py: > get rid of the *Config() classes, directly init and fill out AI, Doot and   Quote after their yaml sections are parsed. > directly init the Server and its socket in here. > init your own logger.

--- bands/config.py
+++ bands/config.py
@@ -3,121 +3,35 @@ import os
 
 import yaml
 
+from .ai import AI
+from .doot import Doot
+from .irc.server import Server
+from .irc.socket import Socket
+from .log import set_logger
+from .quote import Quote
+
 
 # pylint: disable=too-many-instance-attributes,too-few-public-methods
-class ServerConfig:
-    def __init__(self):
-        self.name = None
-        self.address = None
-        self.port = None
-        self.botname = None
-        self.channels = []
-        self.allow_admin = None
-        self.secret = None
-
-        self.passwd = None
-        self.tls = None
-        self.verify_tls = None
-        self.scroll_speed = None
-
-
-class OpenAIConfig:
-    def __init__(self):
-        self.keys = None
-
-
-class QuoteConfig:
-    def __init__(self):
-        self.file = None
-
-
-class DootConfig:
-    def __init__(self):
-        self.file = None
-
-
 class ConfigYAML:
-    def __init__(self, config_file):
+    def __init__(self, config_file, debug=False):
         self.config_file = config_file
-
-        self.logger = None
-
-        self.openai = None
-        self.quote = None
-        self.doot = None
-        self.servers = []
+        self.debug = debug
 
         self.yaml_parsed = None
 
-    def _parse_doot(self):
-        self.logger.info("processing doot file")
+        self.logger = None
+        self.servers = []
 
-        try:
-            doot_file = self.yaml_parsed["doot_file"]
-        # pylint: disable=bare-except
-        except:
-            self.logger.exception("%s parsing has failed", self.config_file)
-
-        if not doot_file:
-            self.logger.error("doot_file cannot be left blank")
-
-        if not os.path.isfile(doot_file):
-            self.logger.warning("%s is not a file, creating", doot_file)
-
-            with open(doot_file, "w", encoding="utf-8") as file:
-                file.write(json.dumps({"doots": [{}]}))
-
-        self.logger.info("sanity checking the doot file formatting")
-
-        try:
-            with open(doot_file, "r", encoding="utf-8") as file:
-                doots = json.loads(file.read())
-        # pylint: disable=bare-except
-        except:
-            self.logger.exception("parsing %s failed", doot_file)
-
-        if "doots" not in doots.keys():
-            self.logger.error("%s is formatted wrong", doot_file)
-
-        self.doot = DootConfig()
-        self.doot.file = doot_file
-
-    def _parse_quote(self):
-        self.logger.info("processing quote file")
-
-        try:
-            quote_file = self.yaml_parsed["quote_file"]
-        # pylint: disable=bare-except
-        except:
-            self.logger.exception("%s parsing has failed", self.config_file)
-
-        if not quote_file:
-            self.logger.error("quote_file key cannot blank")
-
-        if not os.path.isfile(quote_file):
-            self.logger.warning("%s is not a file, creating", quote_file)
-
-            with open(quote_file, "w", encoding="utf-8") as file:
-                file.write(json.dumps({"quotes": [{}]}))
-
-        self.logger.info("sanity checking the quote file formatting")
-
-        try:
-            with open(quote_file, "r", encoding="utf-8") as file:
-                quotes = json.loads(file.read())
-        # pylint: disable=bare-except
-        except:
-            self.logger.exception("parsing %s failed", quote_file)
-
-        if "quotes" not in quotes.keys():
-            self.logger.error("%s is formatted wrong", quote_file)
-
-        self.quote = QuoteConfig()
-        self.quote.file = quote_file
+        self.ai = None
+        self.quote = None
+        self.doot = None
 
     def _parse_openai(self):
-        self.logger.info("processing openai_key_file")
+        # - - init AI() - - #
+        self.ai = AI(self.debug)
 
+        # - - parse yaml - - #
+        self.logger.info("processing openai_key_file")
         try:
             openai_key_file = self.yaml_parsed["openai_key_file"]
         except KeyError:
@@ -139,8 +53,8 @@ class ConfigYAML:
         if not os.path.isfile(openai_key_file):
             self.logger.error("%s is not a file", openai_key_file)
 
+        # - - sanity checks - - #
         self.logger.info("sanity checking the openai key file formatting")
-
         try:
             with open(openai_key_file, "r", encoding="utf-8") as file:
                 openai_keys = json.loads(file.read())["openai_keys"]
@@ -158,135 +72,227 @@ class ConfigYAML:
             except KeyError:
                 self.logger.exception("%s formatting is incorrect", openai_key_file)
 
-        self.openai = OpenAIConfig()
-        self.openai.keys = openai_keys
+        # - - append keys - - #
+        self.ai.keys = openai_keys
+
+    def _parse_quote(self):
+        # - - init Quote() - - #
+        self.quote = Quote(self.debug)
+
+        # - - parse yaml - - #
+        self.logger.info("processing quote file")
+        try:
+            quote_file = self.yaml_parsed["quote_file"]
+        # pylint: disable=bare-except
+        except:
+            self.logger.exception("%s parsing has failed", self.config_file)
+
+        if not quote_file:
+            self.logger.error("quote_file key cannot blank")
+
+        if not os.path.isfile(quote_file):
+            self.logger.warning("%s is not a file, creating", quote_file)
+
+            with open(quote_file, "w", encoding="utf-8") as file:
+                file.write(json.dumps({"quotes": [{}]}))
+
+        # - - sanity checks - - #
+        self.logger.info("sanity checking the quote file formatting")
+        try:
+            with open(quote_file, "r", encoding="utf-8") as file:
+                quotes = json.loads(file.read())
+        # pylint: disable=bare-except
+        except:
+            self.logger.exception("parsing %s failed", quote_file)
+
+        if "quotes" not in quotes.keys():
+            self.logger.error("%s is formatted wrong", quote_file)
+
+        # - - set quote file - - #
+        self.quote.file = quote_file
+
+    def _parse_doot(self):
+        # - - init Doot() - - #
+        self.doot = Doot(self.debug)
+
+        # - - parse yaml - - #
+        self.logger.info("processing doot file")
+        try:
+            doot_file = self.yaml_parsed["doot_file"]
+        # pylint: disable=bare-except
+        except:
+            self.logger.exception("%s parsing has failed", self.config_file)
+
+        if not doot_file:
+            self.logger.error("doot_file cannot be left blank")
+
+        if not os.path.isfile(doot_file):
+            self.logger.warning("%s is not a file, creating", doot_file)
+
+            with open(doot_file, "w", encoding="utf-8") as file:
+                file.write(json.dumps({"doots": [{}]}))
+
+        # - - sanity checks - - #
+        self.logger.info("sanity checking the doot file formatting")
+        try:
+            with open(doot_file, "r", encoding="utf-8") as file:
+                doots = json.loads(file.read())
+        # pylint: disable=bare-except
+        except:
+            self.logger.exception("parsing %s failed", doot_file)
+
+        if "doots" not in doots.keys():
+            self.logger.error("%s is formatted wrong", doot_file)
+
+        # - - set doot file - - #
+        self.doot.file = doot_file
 
     # pylint: disable=too-many-branches,too-many-statements
     def _parse_servers(self):
-        # servers
         self.logger.info("processing servers")
 
+        # check if servers key exists
         try:
             servers = self.yaml_parsed["servers"]
         except KeyError:
             self.logger.exception("servers section in the YAML file is missing")
 
+        # check for root keys that must be present
         server_must_have = ["name", "address", "port", "botname", "channels"]
 
-        for server in servers:
+        for server_yaml in servers:
             for item in server_must_have:
-                if item not in server.keys():
+                if item not in server_yaml.keys():
                     self.logger.error("%s is missing from the YAML", item)
-                if not server[item]:
+                if not server_yaml[item]:
                     self.logger.error("%s cannot be blank", item)
 
-            svconf = ServerConfig()
+            # - - socket - - #
+            self.logger.info("generating Socket() for %s", server_yaml["name"])
+            socket = Socket()
 
-            # server.name
-            svconf.name = str(server["name"])
+            # socket.address
+            socket.address = str(server_yaml["address"])
 
-            # server.address
-            svconf.address = str(server["address"])
-
-            # server.port
+            # socket.port
             try:
-                svconf.port = int(server["port"])
+                socket.port = int(server_yaml["port"])
             except ValueError:
                 self.logger.exception("invalid server port")
 
-            if svconf.port <= 0 or svconf.port > 65535:
-                self.logger.error("%s is not a valid port number.", svconf.port)
+            if socket.port <= 0 or socket.port > 65535:
+                self.logger.error("%s is not a valid port number.", socket.port)
 
-            # server.botname
-            svconf.botname = str(server["botname"])
-
-            # server.channels
-            if len(server["channels"]) == 0:
-                self.logger.error("no channels provided for server %s", server["name"])
-
-            for channel in server["channels"]:
-                if channel[0] != "#":
-                    self.logger.error("channels should start with a #")
-
-                try:
-                    svconf.channels.append(str(channel))
-                except ValueError:
-                    self.logger.exception("invalid channel name: %s", channel)
-
-            # server.allow_admin
+            # socket.tls
             try:
-                if type(server["allow_admin"]).__name__ != "bool":
-                    self.logger.error("allow_admin should be a bool")
-            except KeyError:
-                pass
-
-            try:
-                svconf.allow_admin = server["allow_admin"]
-            except KeyError:
-                svconf.allow_admin = False
-
-            # server.secret
-            try:
-                svconf.secret = str(server["secret"])
-            except KeyError:
-                pass
-
-            if svconf.secret == "None":
-                self.logger.error("server secret cannot be specified then left blank")
-
-            # wanted auth bot no secret provided
-            if svconf.allow_admin and not svconf.secret:
-                self.logger.error("allow_admin enabled but no secret provided")
-
-            if svconf.secret and not svconf.allow_admin:
-                self.logger.error("secret provided without enabling allow_admin")
-
-            # server.passwd
-            try:
-                svconf.passwd = str(server["passwd"])
-            except KeyError:
-                pass
-
-            if svconf.passwd == "None":
-                self.logger.error("server passwd cannot be specified then left blank")
-
-            # server.tls
-            try:
-                if type(server["tls"]).__name__ != "bool":
+                if type(server_yaml["tls"]).__name__ != "bool":
                     self.logger.error("tls should be a bool")
             except KeyError:
                 pass
 
             try:
-                svconf.tls = server["tls"]
+                socket.tls = server_yaml["tls"]
             except KeyError:
-                svconf.tls = False
+                socket.tls = False
 
-            # server.verify_tls
+            # socket.verify_tls
             try:
-                if type(server["verify_tls"]).__name__ != "bool":
+                if type(server_yaml["verify_tls"]).__name__ != "bool":
                     self.logger.error("verify_tls should be a bool")
             except KeyError:
                 pass
 
             try:
-                svconf.verify_tls = server["verify_tls"]
+                socket.verify_tls = server_yaml["verify_tls"]
             except KeyError:
-                svconf.verify_tls = False
+                socket.verify_tls = False
+
+            # - - server - - #
+            self.logger.info("generating Server() for %s", server_yaml["name"])
+            server = Server(socket)
+
+            # server.name
+            server.name = str(server_yaml["name"])
+
+            # server.botname
+            server.botname = str(server_yaml["botname"])
+
+            # server.channels
+            if len(server_yaml["channels"]) == 0:
+                self.logger.error("no channels provided for server %s", server.name)
+
+            for channel in server_yaml["channels"]:
+                if channel[0] != "#":
+                    self.logger.error("channels should start with a #")
+
+                try:
+                    server.channels.append(str(channel))
+                except ValueError:
+                    self.logger.exception("invalid channel name: %s", channel)
+
+            # server.allow_admin
+            try:
+                if type(server_yaml["allow_admin"]).__name__ != "bool":
+                    self.logger.error("allow_admin should be a bool")
+            except KeyError:
+                pass
+
+            try:
+                server.allow_admin = server_yaml["allow_admin"]
+            except KeyError:
+                server.allow_admin = False
+
+            # server.secret
+            try:
+                if server_yaml["secret"] is None:
+                    err_msg = "server secret cannot be specified then left blank"
+                    self.logger.error(err_msg)
+                else:
+                    server.secret = str(server_yaml["secret"])
+            except KeyError:
+                pass
+
+            # wanted auth bot no secret provided
+            if server.allow_admin and not server.secret:
+                self.logger.error("allow_admin enabled but no secret provided")
+
+            if server.secret and not server.allow_admin:
+                self.logger.error("secret provided without enabling allow_admin")
+
+            # server.passwd
+            try:
+                if server_yaml["passwd"] is None:
+                    err_msg = "server passwd cannot be specified then left blank"
+                    self.logger.error(err_msg)
+                else:
+                    server.passwd = str(server_yaml["passwd"])
+            except KeyError:
+                pass
 
             # server.scroll_speed
             try:
-                svconf.scroll_speed = int(server["scroll_speed"])
+                server.scroll_speed = int(server_yaml["scroll_speed"])
             except ValueError:
                 self.logger.exception("invalid server scroll_speed")
             except KeyError:
-                svconf.scroll_speed = 0
+                server.scroll_speed = 0
 
-            self.logger.info("generated ServerConfig() for %s", svconf.name)
-            self.servers.append(svconf)
+            # - - pass the rest - - #
+            server.ai = self.ai
+            server.quote = self.quote
+            server.doot = self.doot
+            server.config = self
+
+            # - - logger - - #
+            logger = set_logger(server.name, self.debug)
+            server.socket.logger = logger
+            server.logger = logger
+
+            # - - append - - #
+            self.servers.append(server)
 
     def parse_yaml(self):
-        # yaml->dict
+        self.logger = set_logger(__name__, self.debug)
         self.logger.info("loading yaml")
 
         if os.path.isfile(self.config_file):
@@ -300,6 +306,9 @@ class ConfigYAML:
             self.logger.error("%s is not a file", self.config_file)
 
         self._parse_openai()
+        self.ai.rotate_key()
+
         self._parse_quote()
         self._parse_doot()
+
         self._parse_servers()
