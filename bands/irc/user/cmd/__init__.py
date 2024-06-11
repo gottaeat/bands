08a12870a90eb6e4273dac8c0520267376cfae74
must_eat <mss@tutanota.de>
2024-06-11 15:35:31 +0300

    upd: (bands/) irc/user/cmd/__init__.py; do not reference OpenAIHandler.

--- bands/irc/user/cmd/__init__.py
+++ bands/irc/user/cmd/__init__.py
@@ -1,11 +1,9 @@
 from .auth import Auth
 from .help import Help
-from .openai_handler import OpenAIHandler
 from .rcon import RCon
 
 CMDS = {
     ":?auth": Auth,
     ":?help": Help,
-    ":?openai": OpenAIHandler,
     ":?rcon": RCon,
 }
