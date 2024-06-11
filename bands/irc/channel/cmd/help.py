234e62b75b80dedee3ece5049d30894432398bf7
must_eat <mss@tutanota.de>
2024-06-11 06:48:18 +0300

    upd: (bands/) irc/channel/cmd/help.py; expose ?wa in the prompt.

--- bands/irc/channel/cmd/help.py
+++ bands/irc/channel/cmd/help.py
@@ -13,14 +13,15 @@ class Help:
 
     def _run(self):
         # fmt: off
-        msg  = f"{c.WHITE}│ ├ {c.LGREEN}?advice{c.RES} {{nick}}\n"
+        msg =  f"{c.WHITE}│ ├ {c.LGREEN}?advice{c.RES} {{nick}}\n"
         msg += f"{c.WHITE}│ ├ {c.LGREEN}?bands{c.RES}\n"
         msg += f"{c.WHITE}│ ├ {c.LGREEN}?bj{c.RES}     ?bj help\n"
         msg += f"{c.WHITE}│ ├ {c.LGREEN}?doot{c.RES}   ?doot help\n"
         msg += f"{c.WHITE}│ ├ {c.LGREEN}?piss{c.RES}   [nick]\n"
         msg += f"{c.WHITE}│ ├ {c.LGREEN}?quake{c.RES}\n"
         msg += f"{c.WHITE}│ ├ {c.LGREEN}?quote{c.RES}  ?quote help\n"
-        msg += f"{c.WHITE}│ └ {c.LGREEN}?tarot{c.RES}  ?tarot help\n"
+        msg += f"{c.WHITE}│ ├ {c.LGREEN}?tarot{c.RES}  ?tarot help\n"
+        msg += f"{c.WHITE}│ └ {c.LGREEN}?wa{c.RES}     [query]\n"
         msg += f"{c.WHITE}└ {c.LRED}src{c.RES}       https://github.com/gottaeat/bands\n"
         # fmt: on
 
