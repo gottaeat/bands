61f7ae9e625d85f800e2bfdbbe67e78ad6dfa609
must_eat <mss@tutanota.de>
2024-07-08 01:41:22 +0300

    upd: (bands/) util.py; ask for content-type, check mimetype, use specified charset if provided.

--- bands/util.py
+++ bands/util.py
@@ -2,6 +2,7 @@ import urllib.request
 
 
 def get_url(url, tls_context=None):
+    # set useragent
     ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     ua += "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 
@@ -10,18 +11,55 @@ def get_url(url, tls_context=None):
         headers={"User-Agent": ua},
     )
 
+    # get response
+    # pylint: disable=consider-using-with
     if tls_context:
-        with urllib.request.urlopen(request, context=tls_context) as f:
-            data = f.read()
+        response = urllib.request.urlopen(request, context=tls_context)
     else:
-        with urllib.request.urlopen(request) as f:
-            data = f.read()
+        response = urllib.request.urlopen(request)
 
+    # get content-type
+    content_type = response.getheader("Content-Type")
+
+    if not content_type:
+        response.close()
+        raise ValueError("Content-Type header was not found")
+
+    content_type = content_type.split(";")
+
+    # check if we take the mimetype
+    mimetype = content_type[0]
+
+    if mimetype not in (
+        "application/javascript",
+        "application/json",
+        "application/xml",
+        "text/html",
+        "text/javascript",
+    ):
+        response.close()
+        raise ValueError(f'"{mimetype}" is not an accepted mimetype')
+
+    # check if charset was specified
     try:
-        data = data.decode(encoding="UTF-8")
-    except UnicodeDecodeError:
-        data = data.decode(encoding="latin-1")
+        charset = content_type[1].split("charset=")[1]
+    except IndexError:
+        charset = "utf-8"
 
+    # read data
+    data = response.read()
+    response.close()
+
+    # decode data
+    try:
+        data = data.decode(encoding=charset)
+    except:
+        try:
+            data = data.decode(encoding="utf-8")
+        except UnicodeDecodeError:
+            data = data.decode(encoding="latin-1")
+
+    # check data len
     if len(data) == 0:
         raise ValueError("empty data returned")
 
