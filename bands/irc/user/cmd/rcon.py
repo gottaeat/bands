1011721d10a1e7177cadcc1de0da62ad15cd61be
must_eat <mss@tutanota.de>
2024-07-28 01:43:07 +0300

    rwr: (bands/) irc/user/cmd/rcon.py; redo join-part and connect-dc logic, add a dispatcher.

--- bands/irc/user/cmd/rcon.py
+++ bands/irc/user/cmd/rcon.py
@@ -21,173 +21,188 @@ class RCon:
         msg += f"{c.WHITE}├ {c.LGREEN}dc{c.RES}      [list of servers]\n"
         msg += f"{c.WHITE}├ {c.LGREEN}join{c.RES}    [server] [list of channels]\n"
         msg += f"{c.WHITE}├ {c.LGREEN}part{c.RES}    [server] [list of channels]\n"
-        msg += f"{c.WHITE}├ {c.LGREEN}raw{c.RES}     [raw irc line]\n"
+        msg += f"{c.WHITE}├ {c.LGREEN}raw{c.RES}     [server] [raw irc line]\n"
         msg += f"{c.WHITE}├ {c.LGREEN}rehash{c.RES}\n"
         msg += f"{c.WHITE}└ {c.LGREEN}status{c.RES}"
         # fmt: on
         self.user.send_query(msg)
 
-    def _get_objects(self, server_name):
-        # server object
+    def _get_server(self, server_name):
+        server_obj = None
         for server in self.config.servers:
-            if server.name == server_name:
+            if server.name.lower() == server_name.lower():
                 server_obj = server
                 break
 
+        if not server_obj:
+            return
+
+        server_channels = {}
+        for channel in server_obj.channel_obj:
+            server_channels[channel.name] = channel
+
+        if len(server_channels) == 0:
+            server_channels = None
+
+        server_dict = {
+            "name": server_obj.name,
+            "obj": server_obj,
+            "channels": server_channels,
+        }
+
+        return server_dict
+
+    def _dispatcher(self):
+        cmd = self.user_args[0]
+
+        # rehash
+        if cmd == "rehash":
+            self._cmd_rehash()
+            return
+
+        # status
+        if cmd == "status":
+            self._cmd_status()
+            return
+
+        # connect + dc
+        if cmd in ("connect", "dc"):
+            server_names = self.user_args[1:]
+
+            if len(server_names) == 0:
+                self.user.send_query(f"{c.ERR} must supply at least one server.")
+                return
+
+            for server_name in server_names:
+                server_dict = self._get_server(server_name)
+
+                if server_dict is None:
+                    self.user.send_query(f"{c.ERR} {server_name} does not exist.")
+                    continue
+
+                # connect
+                if cmd == "connect":
+                    self._cmd_connect(server_dict["obj"])
+                    continue
+
+                # dc
+                if cmd == "dc":
+                    self._cmd_dc(server_dict["obj"])
+
+            return
+
+        # join + part + raw
+        if cmd in ("join", "part", "raw"):
+            # take input
+            if len(self.user_args) == 1:
+                self.user.send_query(f"{c.ERR} must supply a server as the first arg.")
+                return
+
+            if cmd != "raw" and len(self.user_args) == 2:
+                self.user.send_query(f"{c.ERR} must supply at least one channel.")
+                return
+
+            server_name = self.user_args[1]
+
+            if cmd == "raw":
+                msg = " ".join(self.user_args[2:])
+
+                if len(msg) == 0:
+                    self.user.send_query(f"{c.ERR} no message provided.")
+                    return
+            else:
+                channel_names = self.user_args[2:]
+
+                for channel_name in channel_names:
+                    if channel_name[0] != "#":
+                        self.user.send_query(f"{c.ERR} channels must start with a #.")
+                        return
+
+            # act on input
+            server_dict = self._get_server(server_name)
+            if server_dict is None:
+                self.user.send_query(f"{c.ERR} {server_name} does not exist.")
+                return
+
+            # raw
+            if cmd == "raw":
+                self._cmd_raw(server_dict["obj"], msg)
+                return
+
+            # join + part
+            for channel_name in channel_names:
+                if cmd == "join":
+                    self._cmd_join(server_dict, channel_name)
+                    continue
+
+                if cmd == "part":
+                    self._cmd_part(server_dict, channel_name)
+            return
+
+        self._usage()
+
+    # - - server cmd - - #
+    def _cmd_connect(self, server_obj):
+        if server_obj.socket.conn is not None:
+            err_msg = f"{c.ERR} already connected to {server_obj.name}."
+            self.user.send_query(err_msg)
+            return
+
+        self.user.send_query(f"{c.INFO} connecting to {server_obj.name}")
+        server_thread = Thread(target=server_obj.run)
+        server_thread.start()
+        return
+
+    def _cmd_dc(self, server_obj):
+        if self.user.server == server_obj:
+            err_msg = f"{c.ERR} cannot disconnect from {server_obj.name} "
+            err_msg += "because it is the c2 server."
+            self.user.send_query(err_msg)
+            return
+
+        if server_obj.socket.conn is None:
+            warn_msg = f"{c.WARN} {server_obj.name} has no socket, just "
+            warn_msg += "removing it from the server list."
+            self.user.send_query(warn_msg)
+
+            self.config.servers.remove(server_obj)
+        else:
+            self.user.send_query(f"{c.INFO} disconnecting from {server_obj.name}")
+            server_obj.stop()
+
+    def _cmd_raw(self, server_obj, msg):
+        server_obj.sock_ops.send_raw(msg)
+
+    # - - channel cmd - - #
+    def _cmd_join(self, server_dict, channel_name):
         try:
-            if server_obj:
-                # channel name: channel object
-                sv_chans = {}
-                for chan in server_obj.channel_obj:
-                    sv_chans[chan.name] = chan
-        except NameError:
-            server_obj, sv_chans = False, False
-
-        return server_obj, sv_chans
-
-    def _cmd_connect(self):
-        server_names = self.user_args[1:]
-
-        if len(server_names) == 0:
-            self.user.send_query(f"{c.ERR} must supply at least one server.")
-            return
-
-        sv_list = []
-        sv_objs = {}
-        for server in self.config.servers:
-            sv_list.append(server.name)
-            sv_objs[server.name] = server
-
-        for server in server_names:
-            if server not in sv_list:
-                self.user.send_query(f"{c.ERR} {server} does not exist.")
-                return
-
-            if sv_objs[server].socket.conn is not None:
-                self.user.send_query(f"{c.ERR} already connected to {server}.")
-                return
-
-            self.user.send_query(f"{c.INFO} connecting to {server}")
-            server_thread = Thread(target=sv_objs[server].run)
-            server_thread.start()
-
-    def _cmd_dc(self):
-        server_names = self.user_args[1:]
-
-        if len(server_names) == 0:
-            self.user.send_query(f"{c.ERR} must supply at least one server.")
-            return
-
-        sv_list = []
-        sv_objs = {}
-        for server in self.config.servers:
-            sv_list.append(server.name)
-            sv_objs[server.name] = server
-
-        for server in server_names:
-            if server not in sv_list:
-                self.user.send_query(f"{c.ERR} not connected to {server}.")
-                return
-
-            if self.user.server == sv_objs[server]:
-                err_msg = f"{c.ERR} cannot disconnect from {server} "
-                err_msg += "because it is the c2 server."
+            if channel_name in server_dict["channels"].keys():
+                err_msg = f"{c.ERR} already in {channel_name} in "
+                err_msg += f"{server_dict['obj'].name}."
                 self.user.send_query(err_msg)
                 return
+        except AttributeError:
+            pass
 
-            if sv_objs[server].socket.conn is None:
-                warn_msg = f"{c.WARN} {server} has no socket, just removing it "
-                warn_msg += "from the server list."
-                self.user.send_query(warn_msg)
+        server_dict["obj"].sock_ops.send_join(channel_name)
+        self.user.send_query(f"{c.INFO} sent JOIN for {channel_name}.")
 
-                self.config.servers.remove(sv_objs[server])
-            else:
-                self.user.send_query(f"{c.INFO} disconnecting from {server}")
-                sv_objs[server].stop()
-
-    def _cmd_join(self):
-        # take args
-        if len(self.user_args) == 1:
-            self.user.send_query(f"{c.ERR} must supply a server as the first arg.")
-            return
-
-        if len(self.user_args) == 2:
-            self.user.send_query(f"{c.ERR} must supply at least one channel.")
-            return
-
-        server_name = self.user_args[1]
-        channel_names = self.user_args[2:]
-
-        # we handle only for #channels
-        for channel_name in channel_names:
-            if channel_name[0] != "#":
-                self.user.send_query(f"{c.ERR} channels must start with a #.")
+    def _cmd_part(self, server_dict, channel_name):
+        try:
+            if channel_name not in server_dict["channels"].keys():
+                err_msg = f"{c.ERR} not in {channel_name} "
+                err_msg += f"{server_dict['obj'].name}."
+                self.user.send_query(err_msg)
                 return
-
-        # get server object
-        server_obj, sv_chans = self._get_objects(server_name)
-
-        # no server object
-        if not server_obj:
-            self.user.send_query(f"{c.ERR} not connected to {server_name}.")
+        except AttributeError:
+            err_msg = f"{c.ERR} not in any channels in {server_dict['obj'].name}."
+            self.user.send_query(err_msg)
             return
 
-        # check if channel already exists
-        for channel_name in channel_names:
-            if channel_name in sv_chans.keys():
-                self.user.send_query(
-                    f"{c.ERR} already in {channel_name} in {server_obj.name}."
-                )
-                return
+        server_dict["obj"].sock_ops.send_part(channel_name, "mom said no")
+        self.user.send_query(f"{c.INFO} parted from {channel_name}")
 
-        # join channel(s)
-        for channel_name in channel_names:
-            server_obj.sock_ops.send_join(channel_name)
-            self.user.send_query(f"{c.INFO} sent JOIN for {channel_name}.")
-
-    def _cmd_part(self):
-        # take args
-        if len(self.user_args) == 1:
-            self.user.send_query(f"{c.ERR} must supply a server as the first arg.")
-            return
-
-        if len(self.user_args) == 2:
-            self.user.send_query(f"{c.ERR} must supply at least one channel.")
-            return
-
-        server_name = self.user_args[1]
-        channel_names = self.user_args[2:]
-
-        # get server object
-        server_obj, sv_chans = self._get_objects(server_name)
-
-        # no server object
-        if not server_obj:
-            self.user.send_query(f"{c.ERR} not connected to {server_name}.")
-            return
-
-        # check if channel already exists
-        for channel_name in channel_names:
-            if channel_name not in sv_chans.keys():
-                self.user.send_query(
-                    f"{c.ERR} not in {channel_name} in {server_obj.name}."
-                )
-                return
-
-        # part from channel(s)
-        for channel_name in channel_names:
-            server_obj.sock_ops.send_part(channel_name, "mom said no")
-            self.user.send_query(f"{c.INFO} parted from {channel_name}")
-
-    def _cmd_raw(self):
-        msg = " ".join(self.user_args[1:])
-
-        if len(msg) == 0:
-            self.user.send_query(f"{c.ERR} no message provided.")
-            return
-
-        self.user.sock_ops.send_raw(msg)
-
+    # - - noarg - - #
     def _cmd_rehash(self):
         self.user.send_query(f"{c.INFO} reloading configuration YAML.")
         try:
@@ -265,32 +280,4 @@ class RCon:
             self._usage()
             return
 
-        if self.user_args[0] == "connect":
-            self._cmd_connect()
-            return
-
-        if self.user_args[0] == "dc":
-            self._cmd_dc()
-            return
-
-        if self.user_args[0] == "join":
-            self._cmd_join()
-            return
-
-        if self.user_args[0] == "part":
-            self._cmd_part()
-            return
-
-        if self.user_args[0] == "raw":
-            self._cmd_raw()
-            return
-
-        if self.user_args[0] == "rehash":
-            self._cmd_rehash()
-            return
-
-        if self.user_args[0] == "status":
-            self._cmd_status()
-            return
-
-        self._usage()
+        self._dispatcher()
