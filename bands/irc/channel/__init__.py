50cc61e28b691ac1bbe4094b835522a80b37cd47
must_eat <mss@tutanota.de>
2024-07-30 06:39:47 +0300

    upd: (bands/) irc/{channel,server,user}/; move the classes imported in __init__.py to __init__.py directly and remove the files that the classes were from.

--- bands/irc/channel/__init__.py
+++ bands/irc/channel/__init__.py
@@ -1,1 +1,43 @@
-from .channel import Channel, ChannelUser
+class Channel:
+    def __init__(self, server):
+        self.server = server
+        self.sock_ops = server.sock_ops
+
+        # props
+        self.name = None
+        self.topic_msg = None
+        self.topic_nick = None
+        self.topic_login = None
+        self.topic_tstamp = None
+
+        self.users = {}
+
+        self.char_limit = None
+        self.logger = None
+        self.cmd_tstamp = None
+        self.hook_tstamp = None
+
+    def send_query(self, msg):
+        self.sock_ops.send_query_hook(self.name, self.char_limit, msg)
+
+
+class ChannelUser:
+    def __init__(self):
+        self.nick = None
+        self.login = None
+
+        # modes
+        self.owner = None  # ~
+        self.admin = None  # &
+        self.op = None  # @
+        self.hop = None  # %
+        self.voiced = None  # +
+
+        # context
+        self.tarot_deck = None
+        self.bjack = None
+        self.chats = []
+
+        # doots
+        self.used_doot_tstamp = None
+        self.got_dooted_tstamp = None
--- bands/irc/user/__init__.py
+++ bands/irc/user/__init__.py
@@ -1,1 +1,19 @@
-from .user import User
+class User:
+    def __init__(self, server):
+        self.server = server
+        self.sock_ops = server.sock_ops
+
+        # props
+        self.nick = None
+        self.login = None
+
+        self.char_limit = None
+        self.logger = None
+
+        self.tarot_deck = None
+
+        self.tstamp = None
+        self.bad_pw_attempts = 0
+
+    def send_query(self, msg):
+        self.sock_ops.send_query_hook(self.nick, self.char_limit, msg)
