1599a25915790d7c1811f65871c4b40fed39b7ca
must_eat <eateing@loggedin.net>
2026-05-18 05:21:08 +0300

    rwr: (bands/) irc/channel/cmd/help.py; dynamically generate list off of CMDs, take into account the openai and disablement status of a cmd in a channel.

--- bands/irc/channel/cmd/help.py
+++ bands/irc/channel/cmd/help.py
@@ -1,5 +1,4 @@
 from bands.colors import MIRCColors
-from bands import __version__ as pkg_version
 
 c = MIRCColors()
 
@@ -13,19 +12,46 @@ class Help:
         self._run()
 
     def _run(self):
-        # fmt: off
-        msg  = f"{c.WHITE}│ ├ {c.LGREEN}?advice{c.RES} {{nick}}\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?ai{c.RES}     [query]\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?bands{c.RES}\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?bj{c.RES}     ?bj help\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?doot{c.RES}   ?doot help\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?piss{c.RES}   [nick]\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?quake{c.RES}\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?quote{c.RES}  ?quote help\n"
-        msg += f"{c.WHITE}│ ├ {c.LGREEN}?tarot{c.RES}  ?tarot help\n"
-        msg += f"{c.WHITE}│ └ {c.LGREEN}?wa{c.RES}     [query]\n"
-        msg += f"{c.WHITE}├ {c.LRED}ver{c.RES}       {pkg_version}\n"
-        msg += f"{c.WHITE}└ {c.LRED}src{c.RES}       https://github.com/gottaeat/bands"
-        # fmt: on
+        from . import CMDS  # pylint: disable=cyclic-import,import-outside-toplevel
+
+        db = self.channel.server.config.db
+        server_name = self.channel.server.name
+        channel_name = self.channel.name
+
+        prefix = db.get_prefix(server_name, channel_name)
+        disabled = set(db.disabled_commands(server_name, channel_name))
+        has_openai = self.channel.server.config.ai is not None
+
+        # get available cmds
+        help_cmds = []
+        for cmd, meta in CMDS.items():
+            usage = meta["usage"]
+
+            if not usage:
+                continue
+
+            if cmd in disabled:
+                continue
+
+            if meta["openai"] and not has_openai:
+                continue
+
+            formatted_usage = usage.format(prefix=prefix)
+            help_cmds.append((cmd, formatted_usage))
+
+        # calculate max width for first column
+        cmd_width = 0
+        for cmd, _ in help_cmds:
+            cmd_text = f"{prefix}{cmd}"
+            cmd_width = max(len(cmd_text), cmd_width)
+
+        # prompt
+        msg = ""
+        for index, (cmd, usage) in enumerate(help_cmds):
+            branch = "└" if index == len(help_cmds) - 1 else "├"
+            msg += (
+                f"{c.WHITE}{branch} {c.LGREEN}{prefix}{cmd:<{cmd_width}}{c.RES} "
+                f"{usage}\n"
+            )
 
         self.channel.send_query(msg)
