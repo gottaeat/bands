c3e5a7c5442b282cd18fce09eff65a31d3b3615b
must_eat <eateing@loggedin.net>
2026-05-18 05:13:31 +0300

    upd: (bands/) config.py: > remove parsing for removed CMD/HOOKs and the json state save backends. > use string assembler. > make openai stuff just take an openai key, create a client, give it to   BandsAI and give BandsAI object to CMD/HOOKs. > add parser for the sqlite db path. > don't run parsing in threads, actually slower.

--- bands/config.py
+++ bands/config.py
@@ -1,18 +1,11 @@
-import json
 import logging
 import os
-import re
-import threading
 
 import openai
-import wolframalpha
 import yaml
 
-from bs4 import BeautifulSoup
-
-from .util import get_url
-from .doot import Doot
-from .quote import Quote
+from .ai import BandsAI
+from .db import BandsDB
 from .irc.server import Server
 from .irc.socket import Socket
 from .colors import ANSIColors
@@ -30,13 +23,8 @@ class ConfigYAML:
         self.yaml_parsed = None
 
         self.servers = {}
-        self.quote = None
-        self.doot = None
-        self.wa_client = None
-        self.openai_client = None
-        self.openai_model = None
-        self.piped_url = "pipedapi.drgns.space"
-        self.sc_client_id = None
+        self.db = None
+        self.ai = None
 
     def load_yaml(self):
         self.logger.info("loading configuration")
@@ -51,75 +39,35 @@ class ConfigYAML:
             self.logger.error("%s is not a file", self.config_file)
 
     def _gen_prompt(self, server):
-        # fmt: off
-        msg  = f"{ac.BWHI}{ac.BRED}socket{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}address      {ac.RES}{server.socket.address}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}port         {ac.RES}{server.socket.port}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}tls          {ac.RES}{server.socket.tls}{ac.RES}\n"
-        msg += f"{ac.BWHI}└ {ac.BGRN}verify_tls   {ac.RES}{server.socket.verify_tls}{ac.RES}\n"
-        msg += f"{ac.BWHI}{ac.BRED}server{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}botname      {ac.RES}{server.botname}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}channels     {ac.RES}{server.channels_init}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}allow_admin  {ac.RES}{server.allow_admin}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}secret       {ac.RES}{server.secret}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}passwd       {ac.RES}{server.passwd}{ac.RES}\n"
-        msg += f"{ac.BWHI}├ {ac.BGRN}scroll_speed {ac.RES}{server.scroll_speed}{ac.RES}\n"
-        msg += f"{ac.BWHI}└ {ac.BGRN}burst_limit  {ac.RES}{server.burst_limit}{ac.RES}"
-        # fmt: on
+        msg = (
+            f"{ac.BWHI}{ac.BRED}socket{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}address      {ac.RES}{server.socket.address}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}port         {ac.RES}{server.socket.port}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}tls          {ac.RES}{server.socket.tls}{ac.RES}\n"
+            f"{ac.BWHI}└ {ac.BGRN}verify_tls   {ac.RES}{server.socket.verify_tls}{ac.RES}\n"
+            f"{ac.BWHI}{ac.BRED}server{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}botname      {ac.RES}{server.botname}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}channels     {ac.RES}{server.channels_init}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}allow_admin  {ac.RES}{server.allow_admin}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}secret       {ac.RES}{server.secret}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}passwd       {ac.RES}{server.passwd}{ac.RES}\n"
+            f"{ac.BWHI}├ {ac.BGRN}scroll_speed {ac.RES}{server.scroll_speed}{ac.RES}\n"
+            f"{ac.BWHI}└ {ac.BGRN}burst_limit  {ac.RES}{server.burst_limit}{ac.RES}"
+        )
 
         for line in msg.split("\n"):
             server.logger.info(line)
 
-    def _gen_sc_client_id(self):
-        # modified version of the client_id generation functionailty of
-        # https://github.com/3jackdaws/soundcloud-lib which is copyright of
-        # Ian Murphy, licensed under MIT.
-        self.logger.info("generating soundcloud client_id")
+    def _parse_openai_key(self):
+        self.logger.info("processing openai_key")
 
         try:
-            data = get_url("https://soundcloud.com/hospitalrecords/polaris-distant")
-        except:
-            self.logger.exception("GET failed")
-
-        try:
-            data = BeautifulSoup(data, "html.parser")
-        except:
-            self.logger.exception("parse failed")
-
-        scripts = data.findAll("script", attrs={"src": True})
-        if not scripts:
-            self.logger.error("no scripts fouund with src attrib")
-
-        scripts_list = []
-        for script in scripts:
-            src = script["src"]
-            if "cookielaw.org" not in src:
-                scripts_list.append(src)
-
-        for script in scripts_list:
-            if isinstance(script, str):
-                try:
-                    script_data = get_url(script)
-                except:
-                    self.logger.exception("script GET failed")
-
-                client_id = re.findall(r"client_id=([a-zA-Z0-9]+)", script_data)
-
-                if client_id:
-                    self.sc_client_id = client_id[0]
-                    break
-
-        if not self.sc_client_id:
-            self.logger.error("no client_id found")
-
-    def _parse_wolfram(self):
-        # - - parse yaml - - #
-        self.logger.info("processing wolfram_api_key")
-        try:
-            wolfram_api_key = self.yaml_parsed["wolfram_api_key"]
+            openai_key = self.yaml_parsed["openai_key"]
         except KeyError:
-            warn_msg = "wolfram_api_key is not specified in the configuration.\n"
-            warn_msg += "functionality requiring wolfram alpha will not work."
+            warn_msg = (
+                "openai_key is not specified in the configuration.\n"
+                "functionality requiring openai will not work."
+            )
 
             for line in warn_msg.split("\n"):
                 self.logger.warning(line)
@@ -128,51 +76,10 @@ class ConfigYAML:
         except:
             self.logger.exception("%s parsing has failed", self.config_file)
 
-        if not wolfram_api_key:
-            self.logger.error("wolfram_api_key cannot be specified then left blank")
+        if not openai_key:
+            self.logger.error("openai_key cannot be specified then left blank")
 
-        # - - set key - - #
-        self.logger.info("creating wolfram alpha client")
-        self.wa_client = wolframalpha.Client(wolfram_api_key)
-
-        # - - sanity check - - #
-        try:
-            self.wa_client.query("temperature in Washington, DC on October 3, 2012")
-        except Exception as exc:
-            if str(exc) == "Error 1: Invalid appid":
-                self.logger.error("wolfram alpha api key is formatted wrong")
-            else:
-                self.logger.exception("error sanity checking the wolfram alpha api key")
-
-    def _parse_openai(self):
-        # - - parse yaml - - #
-        self.logger.info("processing openai")
-        try:
-            openai_yaml = self.yaml_parsed["openai"]
-        except KeyError:
-            warn_msg = "openai is not specified in the configuration.\n"
-            warn_msg += "functionality requiring openai will not work."
-
-            for line in warn_msg.split("\n"):
-                self.logger.warning(line)
-
-            return
-        except:
-            self.logger.exception("%s parsing has failed", self.config_file)
-
-        if not openai_yaml:
-            self.logger.error("openai cannot be specified then left blank")
-
-        # - - openai.key - - #
-        if "key" not in openai_yaml.keys():
-            self.logger.error("openai.key is missing from the YAML")
-
-        if not openai_yaml["key"]:
-            self.logger.error("openai.key cannot be blank")
-
-        openai_key = openai_yaml["key"]
-
-        self.logger.info("sanity checking openai.key")
+        self.logger.info("sanity checking openai_key")
 
         try:
             if openai_key[0:3] != "sk-":
@@ -180,172 +87,32 @@ class ConfigYAML:
         except KeyError:
             self.logger.exception("%s is formatted wrong", openai_key)
 
-        # - - create client - - #
         self.logger.info("creating openai client")
-        self.openai_client = openai.OpenAI(api_key=openai_key)
+        openai_client = openai.OpenAI(api_key=openai_key)
 
-        # - - get list of models - - #
-        self.logger.info("getting list of models")
+        self.logger.info("creating BandsAI")
+        self.ai = BandsAI(self.logger, openai_client)
+
+    def _parse_db(self):
+        self.logger.info("processing sqlite_file")
+
         try:
-            models = self.openai_client.models.list().dict()["data"]
-        except:
-            self.logger.exception("could not get list of models")
-
-        non_text_match = [
-            "embedding",
-            "vision",
-            "tts",
-            "whisper",
-            "dall",
-            "audio",
-            "realtime",
-            "moderation",
-        ]
-        models_list = sorted(
-            [
-                model["id"]
-                for model in models
-                if not any(keyword in model["id"] for keyword in non_text_match)
-            ]
-        )
-
-        if not models_list:
-            self.logger.error("models list retrieved from OpenAI is empty")
-
-        # - - openai.model - - #
-        if "model" not in openai_yaml.keys():
-            self.logger.error("openai.model is missing from the YAML")
-
-        if not openai_yaml["model"]:
-            self.logger.error("openai.model cannot be blank")
-
-        openai_model = openai_yaml["model"]
-
-        if openai_model not in models_list:
-            warn_msg = f"{openai_model} is not valid, model should be one of:\n"
-            for model in models_list:
-                warn_msg += f" - {model}\n"
-
-            for line in warn_msg.rstrip("\n").split("\n"):
-                self.logger.warning(line)
-
-            self.logger.error("invalid model")
-
-        # - - set model - - #
-        self.openai_model = openai_model
-
-    def _parse_piped(self):
-        # - - parse yaml - - #
-        self.logger.info("processing piped_url")
-        try:
-            piped_url = self.yaml_parsed["piped_url"]
+            sqlite_file = self.yaml_parsed["sqlite_file"]
         except KeyError:
-            warn_msg = "piped_url is not specified in the configuration.\n"
-            warn_msg += f"will use the fallback value of {self.piped_url}"
+            sqlite_file = "/data/bands.sqlite3"
 
-            for line in warn_msg.split("\n"):
-                self.logger.warning(line)
-
-            return
-        except:
-            self.logger.exception("%s parsing has failed", self.config_file)
-
-        if not piped_url:
-            self.logger.error("piped_url cannot be specified then left blank")
-
-        if not bool(
-            re.compile(r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,})+$").match(
-                piped_url
+            self.logger.warning(
+                "sqlite_file key was not specified, using /data/bands.sqlite3"
             )
-        ):
-            self.logger.error("%s is not a fqdn", piped_url)
 
-        self.piped_url = piped_url
+        if not sqlite_file:
+            self.logger.error("sqlite_file cannot be left blank")
 
-    def _parse_quote(self):
-        # - - init Quote() - - #
-        self.quote = Quote(self.logger)
-
-        # - - parse yaml - - #
-        self.logger.info("processing quote file")
-        try:
-            quote_file = self.yaml_parsed["quote_file"]
-        except KeyError:
-            quote_file = "/data/quotes.json"
-
-            warn_msg = "quote_file key was not specified, using /data/quotes.json"
-            self.logger.warning(warn_msg)
-
-        if not quote_file:
-            self.logger.error("quote_file key cannot blank")
-
-        if not os.path.isfile(quote_file):
-            self.logger.warning("%s is not a file, creating", quote_file)
-
-            try:
-                with open(quote_file, "w", encoding="utf-8") as file:
-                    file.write(json.dumps({"quotes": [{}]}))
-            except FileNotFoundError:
-                self.logger.exception("could not create %s", quote_file)
-
-        # - - sanity checks - - #
-        self.logger.info("sanity checking the quote file formatting")
-        try:
-            with open(quote_file, "r", encoding="utf-8") as file:
-                quotes = json.loads(file.read())
-        except:
-            self.logger.exception("parsing %s failed", quote_file)
-
-        if "quotes" not in quotes.keys():
-            self.logger.error("%s is formatted wrong", quote_file)
-
-        # - - set quote file - - #
-        self.quote.file = quote_file
-
-    def _parse_doot(self):
-        # - - init Doot() - - #
-        self.doot = Doot(self.logger)
-
-        # - - parse yaml - - #
-        self.logger.info("processing doot file")
-        try:
-            doot_file = self.yaml_parsed["doot_file"]
-        except KeyError:
-            doot_file = "/data/doots.json"
-
-            warn_msg = "doot_file key was not specified, using /data/doots.json."
-            self.logger.warning(warn_msg)
-
-        if not doot_file:
-            self.logger.error("doot_file cannot be left blank")
-
-        if not os.path.isfile(doot_file):
-            self.logger.warning("%s is not a file, creating", doot_file)
-
-            try:
-                with open(doot_file, "w", encoding="utf-8") as file:
-                    file.write(json.dumps({"doots": [{}]}))
-            except FileNotFoundError:
-                self.logger.exception("could not create %s", doot_file)
-
-        # - - sanity checks - - #
-        self.logger.info("sanity checking the doot file formatting")
-        try:
-            with open(doot_file, "r", encoding="utf-8") as file:
-                doots = json.loads(file.read())
-        except:
-            self.logger.exception("parsing %s failed", doot_file)
-
-        if "doots" not in doots.keys():
-            self.logger.error("%s is formatted wrong", doot_file)
-
-        # - - set doot file - - #
-        self.doot.file = doot_file
+        self.db = BandsDB(self.logger, sqlite_file)
 
     def parse_servers(self):
         self.logger.info("processing servers")
 
-        # check if servers key exists
         try:
             servers = self.yaml_parsed["servers"]
         except KeyError:
@@ -446,8 +213,9 @@ class ConfigYAML:
             # server.secret
             try:
                 if server_yaml["secret"] is None:
-                    err_msg = "server secret cannot be specified then left blank"
-                    self.logger.error(err_msg)
+                    self.logger.error(
+                        "server secret cannot be specified then left blank"
+                    )
                 else:
                     server.secret = str(server_yaml["secret"])
             except KeyError:
@@ -463,8 +231,9 @@ class ConfigYAML:
             # server.passwd
             try:
                 if server_yaml["passwd"] is None:
-                    err_msg = "server passwd cannot be specified then left blank"
-                    self.logger.error(err_msg)
+                    self.logger.error(
+                        "server passwd cannot be specified then left blank"
+                    )
                 else:
                     server.passwd = str(server_yaml["passwd"])
             except KeyError:
@@ -489,9 +258,10 @@ class ConfigYAML:
             if (server.burst_limit == 0 and server.scroll_speed != 0) or (
                 server.scroll_speed == 0 and server.burst_limit != 0
             ):
-                err_msg = "burst_limit/scroll_speed both have to be non-0 "
-                err_msg += "values if one of them is defined as a non-0 value"
-                self.logger.error(err_msg)
+                self.logger.error(
+                    "burst_limit/scroll_speed both have to be non-0 values if "
+                    "one of them is defined as a non-0 value"
+                )
 
             # - - logger - - #
             logger = logging.getLogger(server.name)
@@ -508,24 +278,12 @@ class ConfigYAML:
     def run(self):
         self.load_yaml()
 
-        func_threads = []
-        for config_func in [
-            self._gen_sc_client_id,
-            self._parse_wolfram,
-            self._parse_openai,
-            self._parse_piped,
-            self._parse_quote,
-            self._parse_doot,
+        for config_func in (
+            self._parse_openai_key,
+            self._parse_db,
             self.parse_servers,
-        ]:
-            func_thread = threading.Thread(target=config_func)
-            func_threads.append(func_thread)
-
-        for func_thread in func_threads:
+        ):
             if self.halt:
                 break
 
-            func_thread.start()
-
-        for func_thread in func_threads:
-            func_thread.join()
+            config_func()
