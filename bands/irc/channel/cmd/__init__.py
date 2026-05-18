c0cbb9e4e94e21c8566e58296e49226db2ea927f
must_eat <eateing@loggedin.net>
2026-05-18 05:20:06 +0300

    rwr: (bands/) irc/channel/cmd/__init__.py; use the new <cmd> | class + usage + openai dependence format, remove old CMDs.

--- bands/irc/channel/cmd/__init__.py
+++ bands/irc/channel/cmd/__init__.py
@@ -1,25 +1,17 @@
-from .advice import Advice
 from .ai import AIQuery
 from .blackjack import BlackJack
-from .doot import Doot
-from .finance import Finance
 from .help import Help
-from .piss import Piss
-from .quake import Quake
+from .point import Point
 from .quote import Quote
 from .tarot import Tarot
-from .wa import WAQuery
 
+# fmt: off
 CMDS = {
-    ":?advice": Advice,
-    ":?ai": AIQuery,
-    ":?bands": Finance,
-    ":?bj": BlackJack,
-    ":?doot": Doot,
-    ":?help": Help,
-    ":?piss": Piss,
-    ":?quake": Quake,
-    ":?quote": Quote,
-    ":?tarot": Tarot,
-    ":?wa": WAQuery,
+    "ai":    {"class": AIQuery,   "usage": "[query]",            "openai": True},
+    "bj":    {"class": BlackJack, "usage": "{prefix}bj help",    "openai": False},
+    "help":  {"class": Help,      "usage": None,                 "openai": False},
+    "point": {"class": Point,     "usage": "{prefix}point help", "openai": False},
+    "quote": {"class": Quote,     "usage": "{prefix}quote help", "openai": False},
+    "tarot": {"class": Tarot,     "usage": "[question]",         "openai": True},
 }
+# fmt: on
