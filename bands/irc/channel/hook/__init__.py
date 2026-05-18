469eadc95b5b24317b459f9c5e17b73907d2b04e
must_eat <eateing@loggedin.net>
2026-05-18 05:23:33 +0300

    upd: (bands/) irc/channel/hook/__init__.py; use the new <hook> | class + patter format.

--- bands/irc/channel/hook/__init__.py
+++ bands/irc/channel/hook/__init__.py
@@ -1,3 +1,8 @@
 from .url_dispatcher import URLDispatcher
 
-HOOKS = {"url_dispatcher": URLDispatcher}
+HOOKS = {
+    "url_dispatcher": {
+        "class": URLDispatcher,
+        "pattern": r"https?://[^\s]+",
+    },
+}
